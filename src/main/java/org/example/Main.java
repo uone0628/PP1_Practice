96497f5f68f667c496be1702437777564bb7087e
yuwon <yuwon0628@handong.ac.kr>
2023-09-01 17:15:18 +0900

    saved

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Student s1 = new Student("22200492", "윤유원");
-        System.out.println(s1);
+        System.out.println("Hello world!");
         System.out.println(s1);
 
     }
