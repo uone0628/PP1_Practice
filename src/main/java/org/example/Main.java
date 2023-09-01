6800d17d55cb8a8ebea3195d1758d113ee0a10a2
yuwon <yuwon0628@handong.ac.kr>
2023-09-01 17:14:06 +0900

    saved

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Student s1 = new Student("22200492", "윤유원");
         System.out.println(s1);
+        System.out.println(s1);
+
     }
 }
